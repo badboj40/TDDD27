4c33207552d489059f6a5af589f13625990772f3
Gabriel Hult <hultgabriel@live.se>
2023-04-17 17:15:51 +0200

    FIX: images not loading in django build fixed

--- client/src/Components/Movies.js
+++ client/src/Components/Movies.js
@@ -1,52 +1,11 @@
-import { Button } from 'react-bootstrap';
-import { Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-
-import { signOutFromGoogle } from "../Firebase/Firebase";
+import { PageHeader } from './PageHeader';
 
 
 export function MoviesPage() {
   return (
     <div className="Movies">
       <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
-            <Container className='d-flex'>
-                <Navbar.Brand href="/" className='d-flex'>
-                    <img
-                    src='/popcorn.png'
-                    width="45"
-                    height="45"
-                    className="gg-logo"
-                    alt="GGWatch Logo"
-                    />
-                    <h1 className=''>GGWatch</h1>
-                </Navbar.Brand>
-                <Nav>
-                    <Nav.Link href="movies">Movies</Nav.Link>
-                </Nav>
-                <Container className='d-flex justify-content-end'> 
-                    <Form className="d-flex justify-content-strecth">
-                        <Form.Control
-                        type="search"
-                        placeholder="Enter movie"
-                        className="me-2"
-                        aria-label="Search"
-                        />
-                        <Button variant="outline-light" size="sm">Search</Button>
-                    </Form>
-                    <Nav>
-                        <NavDropdown title="Account" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="account">Settings</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="signout">
-                        <Button className="logout-btn" variant="light" onClick={signOutFromGoogle}>
-                            Sign out
-                        </Button>{' '}
-                        </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav> 
-                </Container>
-            </Container>
-        </Navbar>
+        <PageHeader/>
       </header>
     </div>
   )
--- server/settings.py
+++ server/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/build/static')
 ]
 
+MEDIA_URL = "/images/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'build/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
