42af61e6adbc81beb81dfe0c27e10af3a7eac34f
Gabriel Hult <hultgabriel@live.se>
2023-04-24 21:28:57 +0200

    adds 'add to watchlist' button so watchlist page can be made and used

--- client/src/Components/SearchResult.js
+++ client/src/Components/SearchResult.js
@@ -1,13 +1,30 @@
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { Card, Container } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+import { Card, Container, OverlayTrigger, ToggleButton, Tooltip } from 'react-bootstrap'
 
 export function SearchResultPage() {
     console.log("Load 'SearchResult' page");
     const { searchTerm } = useParams();
     const searchResult = useSelector(state => state.searchTerm);
+    const [checked, setChecked] = useState(Array(searchResult.length).fill(false));
 
-    console.log(searchResult)
+
+    const renderTooltip = (index) => {
+        if (index) {
+            return (
+                <Tooltip id="button-tooltip">
+                    Remove from my watchlist
+                </Tooltip>
+            );
+        } else {
+            return (
+                <Tooltip id="button-tooltip">
+                    Add to my watchlist
+                </Tooltip>
+            );
+        }
+    };
 
     return (
         <div className="SearchResult">
@@ -20,6 +37,26 @@ export function SearchResultPage() {
                                 <Card.Title>{item.results.title}</Card.Title>
                                 <Card.Text>{item.results.plot}</Card.Text>
                                 <Card.Text>Movie length: {item.results.movie_length}</Card.Text>
+                                <OverlayTrigger
+                                    placement="right"
+                                    delay={{ show: 250, hide: 400 }}
+                                    overlay={renderTooltip(checked[index])}
+                                >
+                                    <ToggleButton
+                                        id={index}
+                                        type="checkbox"
+                                        variant="secondary"
+                                        checked={checked[index]}
+                                        value={index}
+                                        onChange={(e) => {
+                                            const newChecked = [...checked];
+                                            newChecked[index] = e.currentTarget.checked;
+                                            setChecked(newChecked);
+                                        }}
+                                    >
+                                        {checked[index] ? '-' : '+'}
+                                    </ToggleButton>
+                                </OverlayTrigger>
                             </Card.Body>
                         </Card>
                     ))
